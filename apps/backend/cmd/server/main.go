14708b40dff1ca2e5cf8d874819251eec0e967f0
atomisadev <atomdevelops@pm.me>
2025-04-26 13:56:02 +0000

    ur welcome josh

---
+++ apps/backend/cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from the Go API!")
+}
